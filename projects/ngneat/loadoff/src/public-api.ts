d6aeaed17cd6e17339bcfc43293e3855e882a91c
Netanel Basal <NetanelBasal@users.noreply.github.com>
2021-08-03 15:52:25 +0300

    feat: 🎸 add createsync state

--- projects/ngneat/loadoff/src/public-api.ts
+++ projects/ngneat/loadoff/src/public-api.ts
@@ -7,6 +7,7 @@ export {
   isComplete,
   isLoading,
   AsyncState,
+  createSyncState
 } from './lib/toAsyncState';
 export { createAsyncStore } from './lib/asyncStore';
 export { someLoading } from './lib/operators';
