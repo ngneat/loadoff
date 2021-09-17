9229853d0a425b1b74c046b99ddcc537006e75de
Netanel Basal <netanel7799@gmail.com>
2021-09-17 15:34:13 +0300

    fix: 🐛 expose sync state
    ✅ Closes: #11

--- projects/ngneat/loadoff/src/public-api.ts
+++ projects/ngneat/loadoff/src/public-api.ts
@@ -7,6 +7,7 @@ export {
   isComplete,
   isLoading,
   AsyncState,
+  createSyncState,
 } from './lib/toAsyncState';
 export { createAsyncStore } from './lib/asyncStore';
 export { retainResponse } from './lib/retainResponse';
